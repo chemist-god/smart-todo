df2805d7e2eb4471d654b2a67f0029e5b82c06a5
chemist-god <godtenkorang72@gmail.com>
2025-09-22 23:48:48 +0000

    Add authenticated goals pages

---
+++ src/app/(authenticated)/goals/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import GoalList from "@/components/goals/GoalList";
+
+export default function GoalsPage() {
+    return (
+        <div className="space-y-6">
+            <GoalList />
+        </div>
+    );
+}
