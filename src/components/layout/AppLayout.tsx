b1db63b695cd2c2ac3f6ade11d302a168dd079b7
chemist-god <godtenkorang72@gmail.com>
2025-09-23 00:10:12 +0000

    Refactor AppLayout for improved analytics and goals integration

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -3,6 +3,8 @@
 import { useSession } from "next-auth/react";
 import Sidebar from "./Sidebar";
 import AuthStatus from "../auth/AuthStatus";
+import SWRProvider from "../providers/SWRProvider";
+import RealtimeStatus from "../ui/RealtimeStatus";
 
 interface AppLayoutProps {
     children: React.ReactNode;
@@ -37,13 +39,16 @@ export default function AppLayout({ children }: AppLayoutProps) {
     }
 
     return (
-        <div className="flex h-screen bg-gray-50">
-            <Sidebar />
-            <main className="flex-1 overflow-auto">
-                <div className="p-6">
-                    {children}
-                </div>
-            </main>
-        </div>
+        <SWRProvider>
+            <div className="flex h-screen bg-gray-50">
+                <Sidebar />
+                <main className="flex-1 overflow-auto">
+                    <div className="p-6">
+                        {children}
+                    </div>
+                </main>
+                <RealtimeStatus />
+            </div>
+        </SWRProvider>
     );
 }
